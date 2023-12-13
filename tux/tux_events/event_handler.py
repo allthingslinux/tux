f387ed5756d571c6ebb078eb856d7cf933dde769
jwe66 <jwe66@cornell.edu>
2023-12-12 22:05:12 -0500

    [Cleanup] Cleaned up the event_handler.py code

--- tux/tux_events/event_handler.py
+++ tux/tux_events/event_handler.py
@@ -5,33 +5,35 @@ import logging
 
 class EventHandler(commands.Cog):
     def __init__(self, bot, debug=False):
-        '''
+        """
         Constructor for the EventHandler Cog.
 
-        :param bot: The Discord bot instance.
+        :param bot: The instance of the Discord bot.
         :param debug: A flag indicating whether debug mode is enabled.
-        '''
+        """
         self.bot = bot
         self.debug = debug
-        self.setup_logging()
-        self.load_events()
+        self._setup_logging()
+        self._load_events()
 
-    def setup_logging(self):
-        '''
-        Configures the logging settings based on the debug flag.
-        '''
+    def _setup_logging(self):
+        """
+        Configures logging settings based on the debug flag.
+        """
         logging_level = logging.DEBUG if self.debug else logging.INFO
-        logging.basicConfig(level=logging_level,
-                            format='%(asctime)s [%(levelname)s] %(message)s',
-                            datefmt='%Y-%m-%d %H:%M:%S')
+        logging.basicConfig(
+            level=logging_level,
+            format='%(asctime)s [%(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
 
-    def load_events(self):
-        '''
+    def _load_events(self):
+        """
         Dynamically loads event modules from the 'events' directory.
 
         Each event module should be a Python file in the 'events' directory.
         The file name (excluding extension) is considered the event name.
-        '''
+        """
         events_dir = os.path.join(os.path.dirname(__file__), 'events')
 
         for filename in os.listdir(events_dir):
@@ -51,19 +53,19 @@ class EventHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        '''
+        """
         Event handler for the bot being ready.
 
         This function is called when the bot successfully connects to Discord.
-        '''
+        """
         logging.info(f'{self.bot.user} has connected to Discord!')
 
 
 def setup(bot, debug=False):
-    '''
+    """
     Sets up the EventHandler Cog and adds it to the bot.
 
-    :param bot: The Discord bot instance.
+    :param bot: The instance of the Discord bot.
     :param debug: A flag indicating whether debug mode is enabled.
-    '''
+    """
     bot.add_cog(EventHandler(bot, debug))
