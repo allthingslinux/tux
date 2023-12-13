69bbc8f548d15e7a344fe155d87a0f7c7b6e5727
jwe66 <jwe66@cornell.edu>
2023-12-13 18:48:28 -0500

    [Fix][Feat] Removed discord.py debugging in debug mode

--- tux/main.py
+++ tux/main.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 from tux_events.event_handler import EventHandler
 import asyncio
+import logging
+
+discord_logger = logging.getLogger('discord')
+discord_logger.setLevel(logging.INFO)
 
 bot_prefix = '!'
 intents = discord.Intents.all()
@@ -14,7 +18,7 @@ async def setup():
     bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
     async with bot:
         await event_handler.setup(bot, True)
-        await bot.start('TOKEN',
+        await bot.start('MTE4MjE5NDU4NTY5OTYzMTEzNA.GUaYP5.qbUQSLvBYzZ6TsXP_P3Qx1RZiobPrCDgF3NWpQ',
                         reconnect=True)
 
 
