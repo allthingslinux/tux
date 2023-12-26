81b33d8d23b44b3c3280d62d89d7bc8ea6dd87fa
jwe66 <jwe66@cornell.edu>
2023-12-25 20:24:57 -0500

    [Feat][Tested] Bans.py and merged event_logger

--- tux/main.py
+++ tux/main.py
@@ -3,28 +3,31 @@ from discord.ext import commands
 from tux_events.event_handler import EventHandler
 import asyncio
 import logging
+from tux_utils.tux_logger import setup, TuxLogger
 
-discord_logger = logging.getLogger('discord')
-discord_logger.setLevel(logging.INFO)
+logger = TuxLogger(__name__)
 
 bot_prefix = '!'
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
 
+# Call the setup function once
+asyncio.run(setup(bot, project_logging_level=logging.DEBUG, discord_logging_level=logging.WARNING))
 event_handler = EventHandler(bot, True)
 
 
-async def setup():
-    bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
+async def main():
     async with bot:
+        # Add debugging statements
+        print("Setting up event handler...")
         await event_handler.setup(bot, True)
-        await bot.start('MTE4MjE5NDU4NTY5OTYzMTEzNA.GUaYP5.qbUQSLvBYzZ6TsXP_P3Qx1RZiobPrCDgF3NWpQ',
-                        reconnect=True)
+        print("Event handler setup completed.")
 
+        await bot.start(
+            'TOKEN',
+            reconnect=True
+        )
 
-@bot.event
-async def on_ready():
-    print(f'Bot is ready. Connected as {bot.user}')
-    await setup()
+# Only run asyncio.run(main()) once, as it's the entry point for the application
+asyncio.run(main())
 
-asyncio.run(setup())
