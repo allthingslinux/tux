aa1324f55fe16443df4a9677bc13f2e80cdf5e0e
kaizen <admin@kaizen.wtf>
2024-01-02 13:44:31 -0500

    Add on_guild_channel_create, delete, pins_update, update base event

---
+++ tux/events/on_guild_channel_delete.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from utils._tux_logger import TuxLogger
+
+logger = TuxLogger(__name__)
+
+
+class OnGuildChannelDelete(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel: discord.abc.GuildChannel):
+        """
+        Handles the event when a guild channel gets deleted in a Discord Guild.
+
+        This function is called whenever a guild channel is deleted.
+
+        Note:
+            This function requires the `Intents.guilds` to be enabled. You can get the guild from `guild`.
+
+        Args:
+            channel (discord.abc.GuildChannel): The guild channel that got deleted.
+
+        https://discordpy.readthedocs.io/en/stable/api.html#discord.on_guild_channel_delete
+        """  # noqa E501
+
+        logger.info(f"{channel} has been deleted in {channel.guild}.")
+
+
+async def setup(bot):
+    await bot.add_cog(OnGuildChannelDelete(bot))
---
+++ tux/events/on_guild_channel_pins_update.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import Optional
+
+import discord
+from discord.ext import commands
+from utils._tux_logger import TuxLogger
+
+logger = TuxLogger(__name__)
+
+
+class OnGuildChannelPinsUpdate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_channel_pins_update(
+        self,
+        channel: discord.abc.GuildChannel | discord.Thread,
+        last_pin: datetime | None,
+    ):
+        """
+        Handles the event when a message is pinned or unpinned from a guild channel.
+
+        This function is called whenever a message's pinned status is changed.
+
+        Note:
+            This function requires the `Intents.guilds` to be enabled.
+
+        Args:
+            channel (Union[discord.abc.GuildChannel, discord.Thread]): The guild channel or thread that had its pins updated.
+            last_pin (Optional[datetime.datetime]): The time of the latest message that was pinned, in UTC.
+
+        https://discordpy.readthedocs.io/en/stable/api.html#discord.on_guild_channel_pins_update
+        """  # noqa E501
+
+        logger.info(f"Pins in  #{channel.name} have been updated. Last pin: {last_pin}")
+
+
+async def setup(bot):
+    await bot.add_cog(OnGuildChannelPinsUpdate(bot))
