f00b603746bd2451181e8d4dde7b21b08296d869
kaizen <admin@kaizen.wtf>
2024-01-12 00:14:05 -0500

    [Add] constants.py

---
+++ tux/constants.py
@@ -0,0 +1,20 @@
+# tux/constants.py
+
+# Branding
+BOT_NAME = "Tux"
+BOT_SERVER = "All Things Linux"
+
+# Colors
+C_MAIN = 0xFF006A  # Everything Bot Related
+C_GREEN = 0x15FF00  # Success
+C_RED = 0xFF0000  # Error
+C_ORANGE = 0xFF4400  # Warning
+C_BLUE = 0x00AEFF  # Showing data
+C_TEAL = 0x00EEFF  # Everything else
+
+# Emojis
+E_TUX = "<:tux:1172344387276124251>"
+
+# Roles
+R_MODERATOR = 1172333934051336264
+R_ADMIN = 1182064847052091484
