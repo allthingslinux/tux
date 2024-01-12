6889952718403d5c7cb8fb1ff58afd7524ff7d3a
kaizen <admin@kaizen.wtf>
2024-01-12 12:31:41 -0500

    [Docs] on_app_command_completion.py

--- tux/events/on_app_command_completion.py
+++ tux/events/on_app_command_completion.py
@@ -15,23 +15,37 @@ class OnAppCommandCompletion(commands.Cog):
     @commands.Cog.listener()
     async def on_app_command_completion(
         self,
-        # ctx: commands.Context, <-- is this needed?
         interaction: discord.Interaction,
         command: discord.app_commands.Command | discord.app_commands.ContextMenu,
     ):
         """
         Handles the event when an application command is completed successfully.
 
-        Called when a app_commands.Command or app_commands.ContextMenu has successfully completed without error.
+        This method is called when a discord.app_commands.Command
+        or discord.app_commands.ContextMenu has completed execution
+        successfully without any error.
 
         Args:
-            interaction (discord.Interaction): The Interaction that was used.
-            command (Union[app_commands.Command, app_commands.ContextMenu]): The command that completed successfully
+            interaction (discord.Interaction): The Interaction instance linked with the application command.
+            command (Union[app_commands.Command, app_commands.ContextMenu]): The command that completed execution successfully.
 
-        https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.discord.ext.commands.on_app_command_completion
-        """  # noqa E501
-        logger.info(f"{interaction.user} used {command.name} in {interaction.channel}.")
+        Note:
+        This method logs the details of the application command's use,
+        which includes the user who invoked the command, the command name,
+        and the channel in which it was used.
+
+        https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.on_app_command_completion
+        """
+
+        logger.info(
+            f"'{command.name}' command was used by {interaction.user} in {interaction.channel}."
+        )
 
 
-async def setup(bot):
+async def setup(bot: commands.Bot):
+    """
+    Set up function for loading the 'OnAppCommandCompletion' cog.
+
+    Adds an instance of 'OnAppCommandCompletion' to the bot's cogs.
+    """
     await bot.add_cog(OnAppCommandCompletion(bot))
