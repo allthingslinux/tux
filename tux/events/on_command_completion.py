17d94a768714b48b76b364f745c3bae2c603f1a9
kaizen <admin@kaizen.wtf>
2024-01-12 12:34:00 -0500

    [Docs] on_command_completion.py

--- tux/events/on_command_completion.py
+++ tux/events/on_command_completion.py
@@ -16,18 +16,33 @@ class OnCommandCompletion(commands.Cog):
         """
         Handles the event when a command is completed successfully.
 
-        This function is called when a command is completed successfully.
+        This function is triggered when a command has been executed
+        successfully without any error.
 
         Args:
-            ctx (commands.Context): The Context of the command.
+            ctx (commands.Context): The Context instance of the command.
 
-        https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.discord.ext.commands.on_command_completion
-        """  # noqa E501
+        Note:
+        This function adds a checkmark reaction to the message indicating
+        successful execution and logs the details of the command
+        execution, including the author, the command, and the channel.
 
-        # For testing purposes only, remove this line later.
+        https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.on_command_completion
+        """
+
+        # Add a reaction to the command message indicating its successful completion.
         await ctx.message.add_reaction("✅")
-        logger.info(f"{ctx.author} used {ctx.command} in {ctx.channel}.")
+
+        # Log the successful command execution details.
+        logger.info(
+            f"'{ctx.command}' command was used by {ctx.author} in {ctx.channel}."
+        )
 
 
-async def setup(bot):
+async def setup(bot: commands.Bot):
+    """
+    Set up function for loading the 'OnCommandCompletion' cog.
+
+    Adds an instance of 'OnCommandCompletion' to the bot's cogs.
+    """
     await bot.add_cog(OnCommandCompletion(bot))
