1ce7f6fc148fb961c9db3cdde3a5eb7f97ec05b6
arutonee1 <arutonee1@users.noreply.github.com>
2024-01-26 01:52:51 +0000

    style fixes by ruff

--- tux/commands/temporary_vc.py
+++ tux/commands/temporary_vc.py
@@ -1,23 +1,30 @@
 import discord
-from discord.ext import commands
 import discord.utils
+from discord.ext import commands
 
 from tux.command_cog import CommandCog
-from tux.utils.tux_logger import TuxLogger
-
 from tux.constants import C
+from tux.utils.tux_logger import TuxLogger
 
 logger = TuxLogger(__name__)
 
 
 class TempVC(CommandCog):
     @commands.Cog.listener()
-    async def on_voice_state_update(self, _: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        if before.channel is None: return
-        category = discord.utils.get(before.channel.guild.categories, id=int(C.TEMPVC_CATEGORY or "0") or 0)
-        if category is None: return
-        if before.channel.category_id != category.id: return
-        if before.channel == after.channel: return
+    async def on_voice_state_update(
+        self, _: discord.Member, before: discord.VoiceState, after: discord.VoiceState
+    ):
+        if before.channel is None:
+            return
+        category = discord.utils.get(
+            before.channel.guild.categories, id=int(C.TEMPVC_CATEGORY or "0") or 0
+        )
+        if category is None:
+            return
+        if before.channel.category_id != category.id:
+            return
+        if before.channel == after.channel:
+            return
         await before.channel.delete()
 
     @commands.guild_only()
@@ -27,17 +34,27 @@ class TempVC(CommandCog):
             await ctx.reply(reason)
 
         guild = ctx.guild
-        if guild is None: return await err('Invalid guild. Stop DMing this bot.')
-        category = discord.utils.get(guild.categories, id=int(C.TEMPVC_CATEGORY or "0") or 0)
-        logger.info(f'{C.TEMPVC_CATEGORY} - {list(map(lambda c: c.id, guild.categories))}')
-        if category is None: return await err('Invalid category. This is the bot owner\'s fault.')
+        if guild is None:
+            return await err("Invalid guild. Stop DMing this bot.")
+        category = discord.utils.get(
+            guild.categories, id=int(C.TEMPVC_CATEGORY or "0") or 0
+        )
+        logger.info(
+            f"{C.TEMPVC_CATEGORY} - {list(map(lambda c: c.id, guild.categories))}"
+        )
+        if category is None:
+            return await err("Invalid category. This is the bot owner's fault.")
 
         vcmax = int(C.TEMPVC_MAX or "0") or 0
 
-        if len(category.channels) >= vcmax: return await err(f'The maximum number of VCs ({vcmax}) have already been made.')
+        if len(category.channels) >= vcmax:
+            return await err(
+                f"The maximum number of VCs ({vcmax}) have already been made."
+            )
         await category.create_voice_channel(name)
 
         logger.info(f"{ctx.author} used {ctx.command} in {ctx.channel}.")
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(TempVC(bot))
