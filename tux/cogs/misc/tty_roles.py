99e5ec39720a99ca6a6daf20cb8bd6775fe43d75
kzndotsh <admin@kaizen.wtf>
2024-03-29 10:48:28 -0400

    [Feat] Add TTY roles cog

---
+++ tux/cogs/misc/tty_roles.py
@@ -0,0 +1,57 @@
+import math
+
+import discord
+from discord.ext import commands
+from loguru import logger
+
+
+class TtyRoles(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.base_role_name = "/dev/tty"
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member) -> None:
+        user_count = member.guild.member_count
+        role_name = self.compute_role_name(user_count)
+        role = self.get_role_by_name(member, role_name) or await self.try_create_role(
+            member, role_name
+        )
+        if role:
+            await self.try_assign_role(member, role)
+
+    def compute_role_name(self, user_count: int | None) -> str:
+        if user_count is None:
+            return ""
+        if 1 <= user_count <= 128:
+            return f"{self.base_role_name}0"
+        exponent = int(math.floor(math.log2(user_count)))
+        return self.base_role_name + str(2**exponent)
+
+    @staticmethod
+    def get_role_by_name(member: discord.Member, role_name: str) -> discord.Role | None:
+        return discord.utils.get(member.guild.roles, name=role_name)
+
+    @staticmethod
+    async def try_create_role(member: discord.Member, role_name: str):
+        try:
+            role = await member.guild.create_role(name=role_name)
+            logger.trace(f"Created new role {role_name}")
+            return role
+        except discord.Forbidden:
+            logger.error("I don't have the permissions to create a tty role.")
+        except discord.HTTPException:
+            logger.error("Creating the tty role failed.")
+        return None
+
+    @staticmethod
+    async def try_assign_role(member: discord.Member, role: discord.Role):
+        try:
+            await member.add_roles(role)
+            logger.trace(f"Assigned {role.name} to {member.display_name}")
+        except discord.HTTPException:
+            logger.error(f"Adding tty role failed for {member.display_name}")
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(TtyRoles(bot))
