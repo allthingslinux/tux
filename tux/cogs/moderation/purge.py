5775d09a49059a3304eddb99cfd6217db2d3562f
yaboytabby <86841460+yaboytabby@users.noreply.github.com>
2024-03-31 21:12:14 -0500

    update purge.py

--- tux/cogs/moderation/purge.py
+++ tux/cogs/moderation/purge.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from loguru import logger
 
+
 class Purge(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
@@ -24,24 +25,31 @@ class Purge(commands.Cog):
             text=f"Requested by {interaction.user.display_name}",
             icon_url=interaction.user.display_avatar.url,
         )
-        await interaction.response.send_message(embed=embed)
+        await interaction.followup.send(embed=embed)
 
     @app_commands.checks.has_any_role("Admin", "Sr. Mod", "Mod", "Jr. Mod")
-    @app_commands.command(name="purge", description="Deletes a set number of messages in a channel.")
+    @app_commands.command(
+        name="purge", description="Deletes a set number of messages in a channel."
+    )
     @app_commands.describe(number_messages="The number of messages to be purged.")
-    async def purge_messages(self, interaction: discord.Interaction, number_messages: int = 10) -> None:
+    async def purge_messages(
+        self, interaction: discord.Interaction, number_messages: int = 10
+    ) -> None:
         if not interaction.channel or interaction.channel.type != discord.ChannelType.text:
             return None
 
         if number_messages <= 0:
+            await interaction.response.defer(ephemeral=True)
             return await self.send_embed(
                 interaction,
                 "Error",
                 "The number of messages to purge must be greater than 0.",
                 discord.Colour.red(),
             )
-        # mostly error handling and logging. 
+
         try:
+            await interaction.response.defer(ephemeral=True)
+            await interaction.edit_original_response(content="Purging messages...")
             deleted = await interaction.channel.purge(limit=number_messages)
             description = f"Deleted {len(deleted)} messages in {interaction.channel.mention}"
             await self.send_embed(interaction, "Success!", description, discord.Colour.green())
@@ -66,7 +74,6 @@ class Purge(commands.Cog):
                 discord.Colour.red(),
             )
 
-# ... other methods can be added here ...
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Purge(bot)) 
+    await bot.add_cog(Purge(bot))
