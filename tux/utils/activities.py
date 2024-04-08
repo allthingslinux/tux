6f6693d8e5924dc5c3c6486d854c15fec6d86325
kzndotsh <admin@kaizen.wtf>
2024-04-08 22:59:36 +0000

    refactor(activities.py): remove static member count from activity list to improve accuracy feat(activities.py): add get_member_count method to dynamically fetch member count feat(activities.py): rebuild activity list in each run cycle to reflect updated member count

--- tux/utils/activities.py
+++ tux/utils/activities.py
@@ -8,16 +8,15 @@ class ActivityChanger:
     def __init__(self, bot: commands.Bot, delay: int = 5 * 60):
         self.bot = bot
         self.delay = delay
-        self.activities = self.build_activity_list()
 
     def build_activity_list(self):
         return [
+            discord.Activity(
+                type=discord.ActivityType.watching, name=f"{self.get_member_count()} members"
+            ),
             discord.Streaming(name="fortnite gamer hourz", url="https://twitch.tv/urmom"),
             discord.Activity(type=discord.ActivityType.watching, name="All Things Linux"),
             discord.Activity(type=discord.ActivityType.playing, name="with fire"),
-            discord.Activity(
-                type=discord.ActivityType.watching, name=f"{len(self.bot.users)} members"
-            ),
             discord.Activity(type=discord.ActivityType.watching, name="linux tech tips"),
             discord.Activity(type=discord.ActivityType.listening, name="mpd"),
             discord.Activity(type=discord.ActivityType.watching, name="a vast field of grain"),
@@ -27,8 +26,14 @@ class ActivityChanger:
             ),
         ]
 
+    def get_member_count(self):
+        return sum(len(guild.members) for guild in self.bot.guilds)
+
     async def run(self):
         while True:
+            self.activities = (
+                self.build_activity_list()
+            )  # Build the activity list afresh every cycle
             for activity in self.activities:
                 await self.bot.change_presence(activity=activity)
                 await asyncio.sleep(self.delay)
