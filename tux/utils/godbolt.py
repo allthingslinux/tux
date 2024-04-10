061a4ef370c831ec61bea4def4e5d7cf535679e6
kzndotsh <admin@kaizen.wtf>
2024-04-10 04:40:35 +0000

    feat(pyproject.toml): add httpx library for HTTP requests refactor(eval.py): improve code readability and error handling in eval and genasm commands refactor(godbolt.py): enhance code readability, add type hints, and improve URL construction

--- tux/utils/godbolt.py
+++ tux/utils/godbolt.py
@@ -1,37 +1,74 @@
+from typing import TypedDict
+
 import httpx
+
+
+class CompilerFilters(TypedDict):
+    binary: bool
+    binaryObject: bool
+    commentOnly: bool
+    demangle: bool
+    directives: bool
+    execute: bool
+    intel: bool
+    labels: bool
+    libraryCode: bool
+    trim: bool
+    debugCalls: bool
+
+
+class CompilerOptions(TypedDict):
+    skipAsm: bool
+    executorRequest: bool
+
+
+class Options(TypedDict):
+    userArguments: str
+    compilerOptions: CompilerOptions
+    filters: CompilerFilters
+    tools: list[str]
+    libraries: list[str]
+
+
+class Payload(TypedDict):
+    source: str
+    options: Options
+    lang: str
+    allowStoreCodeDebug: bool
+
 
 client = httpx.Client()
 url = "https://godbolt.org"
 
 
 def checkresponse(res: httpx.Response) -> str | None:
-    if res.status_code == httpx.codes.OK:
-        return res.text
-    return None
+    return res.text if res.status_code == httpx.codes.OK else None
 
 
 def getlanguages() -> str | None:
-    url_lang = url + "/api/languages"
+    url_lang = f"{url}/api/languages"
     response = client.get(url_lang)
     return checkresponse(response)
 
 
 def getcompilers() -> str | None:
-    url_comp = url + "/api/compilers"
+    url_comp = f"{url}/api/compilers"
     response = client.get(url_comp)
     return checkresponse(response)
 
 
 def getspecificcompiler(lang: str) -> str | None:
-    url_comp = url + f"/api/compilers/{lang}"
+    url_comp = f"{url}/api/compilers/{lang}"
     response = client.get(url_comp)
     return checkresponse(response)
 
 
-def getoutput(code: str, lang: str, compileroptions=None) -> str | None:
-    url_comp = url + f"/api/compiler/{lang}/compile"
+def getoutput(code: str, lang: str, compileroptions: str | None = None) -> str | None:
+    url_comp = f"{url}/api/compiler/{lang}/compile"
+
     copt = compileroptions if compileroptions is not None else ""
-    payload = {
+
+    payload: Payload = {
         "source": code,
         "options": {
             "userArguments": copt,
@@ -55,14 +92,18 @@ def getoutput(code: str, lang: str, compileroptions=None) -> str | None:
         "lang": f"{lang}",
         "allowStoreCodeDebug": True,
     }
+
     uri = client.post(url_comp, json=payload)
+
     return checkresponse(uri)
 
 
-def generateasm(code: str, lang: str, compileroptions=None) -> str | None:
-    url_comp = url + f"/api/compiler/{lang}/compile"
+def generateasm(code: str, lang: str, compileroptions: str | None = None) -> str | None:
+    url_comp = f"{url}/api/compiler/{lang}/compile"
+
     copt = compileroptions if compileroptions is not None else ""
-    payload = {
+
+    payload: Payload = {
         "source": code,
         "options": {
             "userArguments": copt,
@@ -88,4 +129,5 @@ def generateasm(code: str, lang: str, compileroptions=None) -> str | None:
     }
 
     uri = client.post(url_comp, json=payload)
+
     return checkresponse(uri)
