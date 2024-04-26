65c61f937bdde8ca5533daae80afef058af9bb05
kzndotsh <admin@kaizen.wtf>
2024-04-26 12:40:02 +0000

    feat(unban.py): add database integration for unbanning users refactor(unban.py): improve error handling and logging for unbanning process style(unban.py): enhance readability and maintainability of the unban command code

--- tux/cogs/moderation/unban.py
+++ tux/cogs/moderation/unban.py
@@ -3,10 +3,66 @@ from discord import app_commands
 from discord.ext import commands
 from loguru import logger
 
+from prisma.models import Infractions
+from tux.database.controllers import DatabaseController
+from tux.utils.embeds import EmbedCreator
+from tux.utils.enums import InfractionType
+
 
 class Unban(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.db_controller = DatabaseController()
+
+    async def insert_infraction(
+        self,
+        user_id: int,
+        moderator_id: int,
+        infraction_type: InfractionType,
+        infraction_reason: str,
+    ) -> Infractions | None:
+        try:
+            return await self.db_controller.infractions.create_infraction(
+                user_id=user_id,
+                moderator_id=moderator_id,
+                infraction_type=infraction_type,
+                infraction_reason=infraction_reason,
+            )
+
+        except Exception as error:
+            logger.error(f"Failed to create infraction for user {user_id}. Error: {error}")
+            return None
+
+    async def get_or_create_user(self, member: discord.User) -> None:
+        user = await self.db_controller.users.get_user_by_id(member.id)
+
+        if not user:
+            await self.db_controller.users.create_user(
+                user_id=member.id,
+                name=member.name,
+                display_name=member.display_name,
+                mention=member.mention,
+                bot=member.bot,
+                created_at=member.created_at,
+                joined_at=None,
+            )
+
+    async def get_or_create_moderator(self, interaction: discord.Interaction) -> None:
+        moderator = await self.db_controller.users.get_user_by_id(interaction.user.id)
+        moderator_context = None
+        if interaction.guild:
+            moderator_context = interaction.guild.get_member(interaction.user.id)
+
+        if not moderator:
+            await self.db_controller.users.create_user(
+                user_id=interaction.user.id,
+                name=interaction.user.name,
+                display_name=interaction.user.display_name,
+                mention=interaction.user.mention,
+                bot=interaction.user.bot,
+                created_at=interaction.user.created_at,
+                joined_at=moderator_context.joined_at if moderator_context else None,
+            )
 
     @app_commands.checks.has_any_role("Admin", "Sr. Mod", "Mod", "Jr. Mod")
     @app_commands.command(name="unban", description="Unbans a member from the server.")
@@ -28,8 +84,8 @@ class Unban(commands.Cog):
             user_to_unban = discord.utils.find(lambda u: u.name == username_or_id, banned_users)
 
         if user_to_unban is None:
-            await self.send_error_message(
-                interaction, "No banned user matches the provided username/ID"
+            await interaction.response.send_message(
+                "User not found in the ban list. Please provide a valid user ID or username."
             )
             return
 
@@ -39,44 +95,43 @@ class Unban(commands.Cog):
 
         try:
             await interaction.guild.unban(user_to_unban, reason=reason)
-            await interaction.response.send_message(
-                embed=self.create_success_embed(
-                    interaction, user_to_unban, reason or "None provided"
-                )
-            )
-        except discord.HTTPException as e:
-            await self.send_error_message(
-                interaction, f"Failed to unban {user_to_unban.display_name}. Error info: {e}"
+
+            await self.get_or_create_user(user_to_unban)
+            await self.get_or_create_moderator(interaction)
+
+            new_unban = await self.insert_infraction(
+                user_id=user_to_unban.id,
+                moderator_id=interaction.user.id,
+                infraction_type=InfractionType.UNBAN,
+                infraction_reason=reason or "No reason provided",
             )
 
-    def create_success_embed(
-        self, interaction: discord.Interaction, user_to_unban: discord.User, reason: str | None
-    ) -> discord.Embed:
-        embed = discord.Embed(
-            title=f"Unbanned {user_to_unban.display_name}!",
-            color=discord.Color.red(),
-            timestamp=interaction.created_at,
-        )
-        embed.add_field(name="Reason", value=f"`{reason}`", inline=True)
-        embed.add_field(name="Member", value=f"<@{user_to_unban.id}>", inline=True)
-        embed.set_footer(
-            text=f"Requested by {interaction.user.display_name}",
-            icon_url=interaction.user.display_avatar,
-        )
-        return embed
+            unban_id = new_unban.id if new_unban else "Unknown"
 
-    async def send_error_message(self, interaction: discord.Interaction, error_msg: str):
-        embed = discord.Embed(
-            color=discord.Color.red(),
-            title="Error",
-            description=error_msg,
-            timestamp=interaction.created_at,
-        )
-        embed.set_footer(
-            text=f"Requested by {interaction.user.display_name}",
-            icon_url=interaction.user.display_avatar,
-        )
-        await interaction.response.send_message(embed=embed)
+            embed = EmbedCreator.create_success_embed(
+                title="Unban",
+                description=f"Successfully unbanned {user_to_unban.display_name}.",
+                interaction=interaction,
+            )
+
+            embed.add_field(name="Action", value="Unban", inline=False)
+            embed.add_field(name="Case ID", value=unban_id, inline=False)
+            embed.add_field(name="Reason", value=reason or "No reason provided", inline=False)
+            embed.add_field(name="Moderator", value=interaction.user.mention, inline=False)
+            embed.add_field(name="User", value=user_to_unban.mention, inline=False)
+
+            await interaction.response.send_message(embed=embed)
+
+        except Exception as error:
+            logger.error(f"Failed to unban user {user_to_unban.display_name}. Error: {error}")
+
+            embed = EmbedCreator.create_error_embed(
+                title="Unban",
+                description=f"Failed to unban {user_to_unban.display_name}.",
+                interaction=interaction,
+            )
+
+            await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
