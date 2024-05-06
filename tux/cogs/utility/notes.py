88668429d6454fcb7a05e1f727de6912ee2c6fd3
yaboytabby <86841460+yaboytabby@users.noreply.github.com>
2024-05-06 18:41:31 -0500

    Added new commands

--- tux/cogs/utility/notes.py
+++ tux/cogs/utility/notes.py
@@ -8,6 +8,8 @@ from tux.database.controllers import DatabaseController
 from tux.utils.embeds import EmbedCreator
 
 # look im going to be real honest with you this code is a mess and I have no idea how this ever worked.
+# I need some *SERIOUS* help fixing this and any and all tips are appreciated.
+# for now though, it works and is that not all that matters?
 
 
 class Note(commands.Cog):
@@ -52,6 +54,22 @@ class Note(commands.Cog):
 
         return await self.table.find_many()
 
+    async def get_note_by_user_id(self, user_id: int) -> Notes | None:
+        """
+        Retrieves a note from the database based on the specified user ID.
+
+        Parameters
+        ----------
+        user_id : int
+            The ID of the note to retrieve.
+
+        Returns
+        -------
+        Notes or None
+            The note if found, otherwise None.
+        """
+        return await self.table.find_first(where={"user_id": user_id})
+
     group = app_commands.Group(name="notes", description="note related commands")
 
     @group.command(name="create", description="Create a note.")
@@ -67,21 +85,55 @@ class Note(commands.Cog):
         embed.add_field(name="Note Content", value=note)
         await interaction.response.send_message(embed=embed)
 
-    @group.command(name="get_all", description="Get all notes in a list.")
-    async def get_all(
+    @group.command(name="get_num", description="Get x notes in a list.")
+    async def get_num(
         self,
         interaction: discord.Interaction,
+        num1: int,
+        num2: int,
     ) -> None:
         notes = await self.get_all_notes()
         embed = EmbedCreator.create_success_embed(
             title="Success!", description="Notes obtained!", interaction=interaction
         )
         result = [(note.content, note.user_id) for note in notes]
-        for i in range(len(notes)):
-            embed.add_field(name="Note #: " + str(i), value=result[i][0], inline=False)
-            embed.add_field(name="Note " + str(i) + "Target", value=[i][1], inline=False)
 
+        try:
+            for i in range(num1, num2):
+                embed.add_field(name="Note #: " + str(i), value=result[i][0], inline=False)
+                embed.add_field(name="Note " + str(i) + " Target", value=result[i][1], inline=False)
+        except IndexError:
+            embed = EmbedCreator.create_error_embed(
+                title="Index Error", description="Out of range", interaction=interaction
+            )
+            embed.add_field(
+                name="Error details",
+                value="One of the numbers you provided is not in the notes list. "
+                "try again with a valid index range.",
+            )
         await interaction.response.send_message(embed=embed)
+
+    @group.command(name="get_by_member", description="Obtain a note based on a certain member.")
+    async def get_by_member(self, interaction: discord.Interaction, member: discord.Member) -> None:
+        member_id = member.id
+        note = await self.get_note_by_user_id(member_id)
+
+        if note is None:
+            embed = EmbedCreator.create_error_embed(
+                title="Error", description="Note not found", interaction=interaction
+            )
+            embed.add_field(
+                name="Error Details",
+                value="Note not found! please try a different member that has a note.",
+            )
+            await interaction.response.send_message(embed=embed)
+        else:
+            embed = EmbedCreator.create_success_embed(
+                title="Note Found!", interaction=interaction, description=""
+            )
+            embed.add_field(name="Note Target", value=str(member))
+            embed.add_field(name="Note content", value=note.content)
+            await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
