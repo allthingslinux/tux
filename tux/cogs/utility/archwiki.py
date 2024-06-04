89ad12eef41337f0abd87023132a1dee5ce86d73
electron271 <66094410+electron271@users.noreply.github.com>
2024-06-03 21:55:31 -0500

    add atl.wiki support to the archwiki cog

--- tux/cogs/utility/archwiki.py
+++ tux/cogs/utility/archwiki.py
@@ -11,6 +11,7 @@ class ArchWiki(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.base_url = "https://wiki.archlinux.org/api.php"
+        self.atl_base_url = "https://atl.wiki/api.php"
 
     def query_archwiki(self, search_term: str) -> tuple[str, str]:
         """
@@ -49,6 +50,43 @@ class ArchWiki(commands.Cog):
             return (data[1][0], data[3][0]) if data[1] else ("error", "error")
         return "error", "error"
 
+    def query_atlwiki(self, search_term: str) -> tuple[str, str]:
+        """
+        Query the atl.wiki API for a search term and return the title and URL of the first search result.
+
+        Parameters
+        ----------
+        search_term : str
+            The search term to query the atl.wiki API with.
+
+        Returns
+        -------
+        tuple[str, str]
+            The title and URL of the first search result.
+        """
+
+        params: dict[str, str] = {
+            "action": "opensearch",
+            "format": "json",
+            "limit": "1",
+            "search": search_term,
+        }
+
+        # Send a GET request to the ArchWiki API
+        with httpx.Client() as client:
+            response = client.get(self.atl_base_url, params=params)
+            logger.info(f"GET request to {self.base_url} with params {params}")
+
+        logger.info(f"URL: {response.url}")
+
+        # example response: ["pacman",["Pacman"],[""],["https://wiki.archlinux.org/title/Pacman"]]
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            return (data[1][0], data[3][0]) if data[1] else ("error", "error")
+        return "error", "error"
+
     @app_commands.command(name="archwiki", description="Search the Arch Wiki.")
     @app_commands.describe(query="The search query.")
     async def archwiki(self, interaction: discord.Interaction, query: str) -> None:
@@ -64,6 +102,36 @@ class ArchWiki(commands.Cog):
         """
 
         title: tuple[str, str] = self.query_archwiki(query)
+
+        if title[0] == "error":
+            embed = EmbedCreator.create_error_embed(
+                title="Error", description="No search results found.", interaction=interaction
+            )
+
+        else:
+            embed = EmbedCreator.create_info_embed(
+                title=title[0],
+                description=title[1],
+                interaction=interaction,
+            )
+
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="atlwiki", description="Search the All Things Linux Wiki.")
+    @app_commands.describe(query="The search query.")
+    async def atlwiki(self, interaction: discord.Interaction, query: str) -> None:
+        """
+        Search the All Things Linux Wiki for a given query and return the title and URL of the first search result.
+
+        Parameters
+        ----------
+        interaction : discord.Interaction
+            The discord interaction object.
+        query : str
+            The search query.
+        """
+
+        title: tuple[str, str] = self.query_atlwiki(query)
 
         if title[0] == "error":
             embed = EmbedCreator.create_error_embed(
