6f999e29c0ebf3eeaa77bc96cf62790929cbfaf8
kzndotsh <admin@kaizen.wtf>
2024-06-14 23:43:56 +0000

    refactor(pyproject.toml, xkcd.py): remove xkcd-wrapper dependency and refactor xkcd cog feat(xkcd.py): improve error handling and logging in xkcd cog style(xkcd.py): improve readability and consistency of xkcd cog code docs(xkcd.py): update command descriptions for clarity and accuracy
    feat(services): add new xkcd service to fetch comics data from xkcd API
    
    This new service includes methods to fetch the latest comic, a specific comic by ID, and a random comic. It also includes a Comic class to represent the fetched comic data.

--- tux/cogs/utility/xkcd.py
+++ tux/cogs/utility/xkcd.py
@@ -1,110 +1,109 @@
 import discord
-import requests
-import xkcd_wrapper
 from discord import app_commands
 from discord.ext import commands
 from loguru import logger
 
+from tux.services import xkcd
 from tux.utils.embeds import EmbedCreator
 
 
-class XKCD(commands.Cog):
-    def __init__(self, bot):
+class Xkcd(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.client = xkcd_wrapper.Client()
+        self.client = xkcd.Client()
 
-    group = app_commands.Group(name="xkcd", description="xkcd commands")
+    group = app_commands.Group(name="xkcd", description="xkcd-related commands")
 
-    @group.command(name="newest", description="Get the latest xkcd comic")
-    async def newest(self, interaction: discord.Interaction) -> None:
+    @group.command(name="latest", description="Get the latest xkcd comic")
+    async def latest(self, interaction: discord.Interaction) -> None:
         try:
-            latest_comic = self.client.latest(raw_comic_image=True)
+            latest_comic = self.client.get_latest_comic(raw_comic_image=True)
+            comic_id = latest_comic.id
+
+            description = f"[Explainxkcd]({latest_comic.explanation_url}) | [Webpage]({latest_comic.comic_url})"
+
             embed = EmbedCreator.create_success_embed(
-                title="Latest xkcd Comic", description=latest_comic.title, interaction=interaction
+                title=f"xkcd {comic_id} - {latest_comic.title}",
+                description=description,
+                interaction=interaction,
             )
+
             embed.set_image(url=latest_comic.image_url)
-            embed.add_field(
-                name="Explainxkcd URL", value=latest_comic.explanation_url, inline=False
-            )
-            embed.add_field(name="Webpage URL", value=latest_comic.comic_url, inline=False)
+
             logger.info(f"{interaction.user} got the latest xkcd comic in {interaction.channel}")
+
             await interaction.response.send_message(embed=embed)
-        except requests.HTTPError:
+
+        except Exception as e:
+            logger.error(f"Error getting the latest xkcd comic: {e}")
+
             embed = EmbedCreator.create_error_embed(
                 title="Error",
-                description="An HTTP error occurred, please try again.",
+                description="An error occurred while fetching the latest xkcd comic",
                 interaction=interaction,
             )
-            await interaction.response.send_message(embed=embed)
-        except requests.Timeout:
-            embed = EmbedCreator.create_error_embed(
-                title="Error",
-                description="A timeout has occoured, please try again.",
-                interaction=interaction,
-            )
-            await interaction.response.send_message(embed=embed)
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @group.command(name="random", description="Get a random xkcd comic")
     async def random(self, interaction: discord.Interaction) -> None:
         try:
-            random_comic = self.client.random(raw_comic_image=True)
+            random_comic = self.client.get_random_comic(raw_comic_image=True)
+            comic_id = random_comic.id
+
+            description = f"[Explainxkcd]({random_comic.explanation_url}) | [Webpage]({random_comic.comic_url})"
+
             embed = EmbedCreator.create_success_embed(
-                title="Random xkcd Comic", description=random_comic.title, interaction=interaction
+                title=f"xkcd {comic_id} - {random_comic.title}",
+                description=description,
+                interaction=interaction,
             )
+
             embed.set_image(url=random_comic.image_url)
-            embed.add_field(
-                name="Explainxkcd URL", value=random_comic.explanation_url, inline=False
-            )
-            embed.add_field(name="Webpage URL", value=random_comic.comic_url, inline=False)
+
+            logger.info(f"{interaction.user} got a random xkcd comic in {interaction.channel}")
+
             await interaction.response.send_message(embed=embed)
-        except requests.HTTPError:
+
+        except Exception as e:
+            logger.error(f"Error getting a random xkcd comic: {e}")
+
             embed = EmbedCreator.create_error_embed(
                 title="Error",
-                description="An HTTP error occurred, please try again.",
+                description="An error occurred while fetching a random xkcd comic",
                 interaction=interaction,
             )
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        except requests.Timeout:
-            embed = EmbedCreator.create_error_embed(
-                title="Error",
-                description="A timeout has occurred, please try again.",
-                interaction=interaction,
-            )
+
             await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @group.command(name="specific", description="Search for a specific xkcd comic")
     async def specific(self, interaction: discord.Interaction, comic_id: int) -> None:
         try:
-            specific_comic = self.client.get(comic_id=comic_id, raw_comic_image=True)
+            specific_comic = self.client.get_comic(comic_id, raw_comic_image=True)
+
+            description = f"[Explainxkcd]({specific_comic.explanation_url}) | [Webpage]({specific_comic.comic_url})"
+
             embed = EmbedCreator.create_success_embed(
-                title=f"xkcd comic {comic_id}",
-                description=specific_comic.title,
+                title=f"xkcd {comic_id} - {specific_comic.title}",
+                description=description,
                 interaction=interaction,
             )
+
             embed.set_image(url=specific_comic.image_url)
-            embed.add_field(
-                name="Explainxkcd URL", value=specific_comic.explanation_url, inline=False
-            )
-            embed.add_field(name="Webpage URL", value=specific_comic.comic_url, inline=False)
+
             await interaction.response.send_message(embed=embed)
-        except requests.HTTPError:
+
+        except Exception as e:
+            logger.error(f"Error getting specific xkcd comic: {e}")
+
             embed = EmbedCreator.create_error_embed(
                 title="Error",
-                description="An HTTP error occurred, please try again.",
+                description="An error occurred while fetching a specific xkcd comic",
                 interaction=interaction,
             )
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        except requests.Timeout:
-            embed = EmbedCreator.create_error_embed(
-                title="Error",
-                description="A timeout has occoured, please try again.",
-                interaction=interaction,
-            )
+
             await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(XKCD(bot))
-
-
-""" Sets up the actual XKCD cog for the bot."""
+    await bot.add_cog(Xkcd(bot))
---
+++ tux/services/xkcd.py
@@ -0,0 +1,311 @@
+import datetime
+import imghdr
+import json
+import random
+from typing import Any
+
+import httpx
+
+
+class HttpError(Exception):
+    def __init__(self, status_code: int, reason: str) -> None:
+        """
+        Initialize the HttpError.
+
+        Parameters
+        ----------
+        status_code : int
+            The status code of the error.
+        reason : str
+            The reason of the error.
+        """
+        self.status_code = status_code
+        self.reason = reason
+        super().__init__(f"HTTP Error {status_code}: {reason}")
+
+
+class Comic:
+    """
+    A class representing an xkcd comic.
+    """
+
+    def __init__(
+        self,
+        xkcd_dict: dict[str, Any],
+        raw_image: bytes | None = None,
+        comic_url: str | None = None,
+        explanation_url: str | None = None,
+    ) -> None:
+        self.id: int | None = xkcd_dict.get("num")
+        self.date: datetime.date | None = self._determine_date(xkcd_dict)
+        self.title: str | None = xkcd_dict.get("safe_title")
+        self.description: str | None = xkcd_dict.get("alt")
+        self.transcript: str | None = xkcd_dict.get("transcript")
+        self.image: bytes | None = raw_image
+        self.image_extension: str | None = self._determine_image_extension()
+        self.image_url: str | None = xkcd_dict.get("img")
+        self.comic_url: str | None = comic_url
+        self.explanation_url: str | None = explanation_url
+
+    def _determine_date(self, xkcd_dict: dict[str, Any]) -> datetime.date | None:
+        """
+        Determine the date of the comic.
+
+        Parameters
+        ----------
+        xkcd_dict : dict[str, Any]
+            The dictionary containing the comic data.
+
+        Returns
+        -------
+        datetime.date | None
+            The date of the comic.
+        """
+        try:
+            return datetime.date(
+                int(xkcd_dict["year"]), int(xkcd_dict["month"]), int(xkcd_dict["day"])
+            )
+
+        except (KeyError, ValueError):
+            return None
+
+    def _determine_image_extension(self) -> str | None:
+        """
+        Determine the image extension of the comic.
+
+        Returns
+        -------
+        str | None
+            The extension of the image.
+        """
+        return f".{imghdr.what(None, h=self.image)}" if self.image else None
+
+    def update_raw_image(self, raw_image: bytes) -> None:
+        """
+        Update the raw image of the comic.
+
+        Parameters
+        ----------
+        raw_image : bytes
+            The raw image data.
+        """
+        self.image = raw_image
+        self.image_extension = self._determine_image_extension()
+
+    def __repr__(self) -> str:
+        """
+        Return the representation of the comic.
+
+        Returns
+        -------
+        str
+            The representation of the comic.
+        """
+        return f"Comic({self.title})"
+
+
+class Client:
+    def __init__(
+        self,
+        api_url: str = "https://xkcd.com",
+        explanation_wiki_url: str = "https://www.explainxkcd.com/wiki/index.php/",
+    ) -> None:
+        self._api_url = api_url
+        self._explanation_wiki_url = explanation_wiki_url
+
+    def latest_comic_url(self) -> str:
+        """
+        Get the URL for the latest comic.
+
+        Returns
+        -------
+        str
+            The URL for the latest comic.
+        """
+        return f"{self._api_url}/info.0.json"
+
+    def comic_id_url(self, comic_id: int) -> str:
+        """
+        Get the URL for a specific comic ID.
+
+        Parameters
+        ----------
+        comic_id : int
+            The ID of the comic.
+
+        Returns
+        -------
+        str
+            The URL for the specific comic ID.
+        """
+        return f"{self._api_url}/{comic_id}/info.0.json"
+
+    def _parse_response(self, response_text: str) -> Comic:
+        """
+        Parse the response text into a Comic object.
+
+        Parameters
+        ----------
+        response_text : str
+            The response text to parse.
+
+        Returns
+        -------
+        Comic
+            The parsed comic object.
+        """
+        response_dict: dict[str, Any] = json.loads(response_text)
+        comic_url: str = f"{self._api_url}/{response_dict['num']}/"
+        explanation_url: str = f"{self._explanation_wiki_url}{response_dict['num']}"
+
+        return Comic(response_dict, comic_url=comic_url, explanation_url=explanation_url)
+
+    def _fetch_comic(self, comic_id: int, raw_comic_image: bool) -> Comic:
+        """
+        Fetch a comic from the xkcd API.
+
+        Parameters
+        ----------
+        comic_id : int
+            The ID of the comic to fetch.
+        raw_comic_image : bool
+            Whether to fetch the raw image data.
+
+        Returns
+        -------
+        Comic
+            The fetched comic.
+        """
+        comic = self._parse_response(self._request_comic(comic_id))
+
+        if raw_comic_image:
+            raw_image = self._request_raw_image(comic.image_url)
+            comic.update_raw_image(raw_image)
+
+        return comic
+
+    def get_latest_comic(self, raw_comic_image: bool = False) -> Comic:
+        """
+        Get the latest xkcd comic.
+
+        Parameters
+        ----------
+        raw_comic_image : bool, optional
+            Whether to fetch the raw image data, by default False
+
+        Returns
+        -------
+        Comic
+            The latest xkcd comic.
+        """
+        return self._fetch_comic(0, raw_comic_image)
+
+    def get_comic(self, comic_id: int, raw_comic_image: bool = False) -> Comic:
+        """
+        Get a specific xkcd comic.
+
+        Parameters
+        ----------
+        comic_id : int
+            The ID of the comic to fetch.
+        raw_comic_image : bool, optional
+            Whether to fetch the raw image data, by default False
+
+        Returns
+        -------
+        Comic
+            The fetched xkcd comic.
+        """
+        return self._fetch_comic(comic_id, raw_comic_image)
+
+    def get_random_comic(self, raw_comic_image: bool = False) -> Comic:
+        """
+        Get a random xkcd comic.
+
+        Parameters
+        ----------
+        raw_comic_image : bool, optional
+            Whether to fetch the raw image data, by default False
+
+        Returns
+        -------
+        Comic
+            The random xkcd comic.
+        """
+        latest_comic_id: int = self._parse_response(self._request_comic(0)).id or 0
+        random_id: int = random.randint(1, latest_comic_id)
+
+        return self._fetch_comic(random_id, raw_comic_image)
+
+    def _request_comic(self, comic_id: int) -> str:
+        """
+        Request the comic data from the xkcd API.
+
+        Parameters
+        ----------
+        comic_id : int
+            The ID of the comic to fetch.
+
+        Returns
+        -------
+        str
+            The response text.
+
+        Raises
+        ------
+        HttpError
+            If the request fails.
+        """
+        comic_url = self.latest_comic_url() if comic_id <= 0 else self.comic_id_url(comic_id)
+
+        try:
+            response = httpx.get(comic_url)
+            response.raise_for_status()
+
+        except httpx.HTTPStatusError as exc:
+            raise HttpError(exc.response.status_code, exc.response.reason_phrase) from exc
+
+        return response.text
+
+    @staticmethod
+    def _request_raw_image(raw_image_url: str | None) -> bytes:
+        """
+        Request the raw image data from the xkcd API.
+
+        Parameters
+        ----------
+        raw_image_url : str | None
+            The URL of the raw image data.
+
+        Returns
+        -------
+        bytes
+            The raw image data.
+
+        Raises
+        ------
+        HttpError
+            If the request fails.
+        """
+        if not raw_image_url:
+            raise HttpError(404, "Image URL not found")
+
+        try:
+            response = httpx.get(raw_image_url)
+            response.raise_for_status()
+
+        except httpx.HTTPStatusError as exc:
+            raise HttpError(exc.response.status_code, exc.response.reason_phrase) from exc
+
+        return response.content
+
+    def __repr__(self) -> str:
+        """
+        Return the representation of the client.
+
+        Returns
+        -------
+        str
+            The representation of the client.
+        """
+        return "Client()"
