951534b83c83c7f91d6c13bbacf7043fc2a6b384
kzndotsh <admin@kaizen.wtf>
2024-07-24 13:00:25 +0000

    refactor(help.py): extract repeated code into helper functions to improve code readability and maintainability feat(help.py): add support for command flag details in help messages to provide more comprehensive command information fix(help.py): correct grammar in error logging message for consistency and clarity

--- tux/help.py
+++ tux/help.py
@@ -1,5 +1,5 @@
 from collections.abc import Mapping
-from typing import Any
+from typing import Any, get_type_hints
 
 import discord
 from discord.ext import commands
@@ -12,7 +12,54 @@ from tux.utils.embeds import EmbedCreator
 class TuxHelp(commands.HelpCommand):
     def __init__(self):
         self.prefix = CONST.PREFIX
-        super().__init__()
+        super().__init__(
+            command_attrs={
+                "help": "Lists all categories and commands.",
+                "aliases": ["h"],
+                "usage": "$help <category> or <command>",
+            },
+        )
+
+    def embed_base(self, title: str, description: str | None = None) -> discord.Embed:
+        return discord.Embed(
+            title=title,
+            description=description,
+            color=CONST.EMBED_COLORS["DEFAULT"],
+        )
+
+    def add_command_field(self, embed: discord.Embed, command: commands.Command[Any, Any, Any], prefix: str):
+        embed.add_field(
+            name=f"{prefix}{command.qualified_name} ({', '.join(command.aliases) if command.aliases else 'No aliases.'})",
+            value=f"> {command.short_doc or 'No documentation summary.'}",
+            inline=False,
+        )
+
+    def format_flag_details(self, command: commands.Command[Any, Any, Any]) -> str:
+        try:
+            type_hints = get_type_hints(command.callback)
+        except Exception:
+            type_hints = {}
+
+        flag_details: list[str] = []
+
+        for param_annotation in type_hints.values():
+            if isinstance(param_annotation, type) and issubclass(param_annotation, commands.FlagConverter):
+                command_flags = param_annotation.__commands_flags__
+                for flag in command_flags.values():
+                    flag_type = flag.annotation if flag.annotation is not None else "Any"
+                    if isinstance(flag_type, type):
+                        flag_type = flag_type.__name__
+                    flag_name_format = f"--[{flag.name}]" if flag.required else f"--<{flag.name}>"
+                    flag_str = f"{flag_name_format}"
+                    if flag.aliases:
+                        flag_str += f" ({', '.join(flag.aliases)})"
+                    flag_str += f"\n\t{flag.description or 'No description provided.'}"
+                    flag_str += f"\n\tType: `{flag_type}`"
+                    if flag.default is not discord.utils.MISSING:
+                        flag_str += f"\n\tDefault: {flag.default}"
+                    flag_str += "\n\n"
+                    flag_details.append(flag_str)
+        return "".join(flag_details)
 
     async def send_bot_help(
         self,
@@ -21,10 +68,7 @@ class TuxHelp(commands.HelpCommand):
         """
         Sends help message for the bot.
         """
-        embed = discord.Embed(
-            title="Categories",
-            color=discord.Color.blurple(),
-        )
+        embed = self.embed_base("Categories")
 
         category_strings: dict[str, str] = {}
 
@@ -49,14 +93,9 @@ class TuxHelp(commands.HelpCommand):
                     for subcommand in command.commands:
                         category_strings[category] += f"`{subcommand.name}` "
 
-        # TODO: normalize spacing between category and command names
-
-        # Set the description of the embed to the category strings
         embed.description = "\n".join(category_strings.values())
 
-        embed.set_footer(
-            text=f"Use {self.prefix}help <category> or {self.prefix}<command> to learn about a category or command.",
-        )
+        embed.set_footer(text=f"Use {self.prefix}help [category] or [command] to learn about it.")
 
         await self.get_destination().send(embed=embed)
 
@@ -64,25 +103,14 @@ class TuxHelp(commands.HelpCommand):
         """
         Sends help message for a cog.
         """
-        embed = discord.Embed(
-            title=f"{cog.qualified_name} Commands",
-            color=discord.Color.blurple(),
-        )
+        embed = self.embed_base(f"{cog.qualified_name} Commands")
 
         for command in cog.get_commands():
-            embed.add_field(
-                name=f"{self.prefix}{command.qualified_name} ({', '.join(command.aliases) if command.aliases else 'No aliases.'})",
-                value=f"> {command.short_doc or 'No documentation summary.'}",
-                inline=False,
-            )
+            self.add_command_field(embed, command, self.prefix)
 
             if isinstance(command, commands.Group):
                 for subcommand in command.commands:
-                    embed.add_field(
-                        name=f"{self.prefix}{subcommand.qualified_name} ({', '.join(subcommand.aliases) if subcommand.aliases else 'No aliases.'})",
-                        value=f"> {subcommand.short_doc or 'No documentation summary.'}",
-                        inline=False,
-                    )
+                    self.add_command_field(embed, subcommand, self.prefix)
 
         await self.get_destination().send(embed=embed)
 
@@ -90,11 +118,11 @@ class TuxHelp(commands.HelpCommand):
         """
         Sends help message for a command.
         """
-        embed = discord.Embed(
+        embed = self.embed_base(
             title=f"{self.prefix}{command.qualified_name}",
             description=f"> {command.help or 'No documentation available.'}",
-            color=discord.Color.blurple(),
         )
+
         embed.add_field(name="Usage", value=f"`{command.signature or 'No usage.'}`", inline=False)
         embed.add_field(
             name="Aliases",
@@ -102,19 +130,19 @@ class TuxHelp(commands.HelpCommand):
             inline=False,
         )
 
+        if flag_details := self.format_flag_details(command):
+            embed.add_field(name="Flags", value=f"```\n{flag_details}\n```", inline=False)
+
         await self.get_destination().send(embed=embed)
 
     async def send_group_help(self, group: commands.Group[Any, Any, Any]):
         """
         Sends help message for a group.
         """
-        embed = discord.Embed(
-            title=f"{group.name}",
-            description=f"> {group.help or 'No documentation available.'}",
-            color=discord.Color.blurple(),
-        )
+        embed = self.embed_base(f"{group.name}", f"> {group.help or 'No documentation available.'}")
 
         embed.add_field(name="Usage", value=f"`{group.signature or 'No usage.'}`", inline=False)
+
         embed.add_field(
             name="Aliases",
             value=f"`{', '.join(group.aliases)}`" if group.aliases else "No aliases.",
@@ -122,16 +150,12 @@ class TuxHelp(commands.HelpCommand):
         )
 
         for command in group.commands:
-            embed.add_field(
-                name=f"{self.prefix}{command.qualified_name} ({', '.join(command.aliases) if command.aliases else 'No aliases.'})",
-                value=f"> {command.short_doc or 'No documentation summary.'}",
-                inline=False,
-            )
+            self.add_command_field(embed, command, self.prefix)
 
         await self.get_destination().send(embed=embed)
 
     async def send_error_message(self, error: str) -> None:
-        logger.error(f"An error occurred while sending help message: {error}")
+        logger.error(f"An error occurred while sending a help message: {error}")
 
         embed = EmbedCreator.create_error_embed(
             title="An error occurred while sending help message.",
