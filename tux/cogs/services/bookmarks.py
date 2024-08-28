632eb570aac9037da49655b777a560712dc0b300
stingleyisme <102343489+stingleyisme@users.noreply.github.com>
2024-08-27 20:47:02 -0500

    new file:   tux/cogs/services/bookmarks.py

---
+++ tux/cogs/services/bookmarks.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from loguru import logger
+
+
+class Bookmarks(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User) -> None:
+        if str(reaction.emoji) == "🔖":
+            message_link = f"https://discord.com/channels/{reaction.message.guild.id}/{reaction.message.channel.id}/{reaction.message.id}"
+
+            try:
+                await user.send(f"Bookmarked: {message_link}")
+            except (discord.Forbidden, discord.HTTPException):
+                logger.error(f"An error occurred while bookmarking {message_link} for {user}")
+                return
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Bookmarks(bot))
