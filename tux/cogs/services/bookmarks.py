f777d08bde62966625ff5ba08ff7b2309f35de2c
stingleyisme <102343489+stingleyisme@users.noreply.github.com>
2024-08-28 17:52:41 -0500

    modified:   tux/cogs/services/bookmarks.py

--- tux/cogs/services/bookmarks.py
+++ tux/cogs/services/bookmarks.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from loguru import logger
 
+from tux.utils.embeds import EmbedCreator
+
 
 class Bookmarks(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -14,16 +16,20 @@ class Bookmarks(commands.Cog):
             message_contents = reaction.message.content
             message_attachments = reaction.message.attachments
             author_username = reaction.message.author.name
+
+            embed = EmbedCreator.create_info_embed(
+                title="Message Bookmarked",
+                description=f"> {message_contents}",
+            )
+            embed.add_field(name="Author", value=author_username, inline=False)
+            embed.add_field(name="Jump to Message", value=f"[Click Here]({message_link})", inline=False)
+
+            if message_attachments:
+                attachments_info = "\n".join([attachment.url for attachment in message_attachments])
+                embed.add_field(name="Attachments", value=attachments_info, inline=False)
+
             try:
-                if message_attachments:
-                    attachments_info = "\n".join([attachment.url for attachment in message_attachments])
-                    await user.send(
-                        f"Bookmarked: {message_link}\nAuthor: {author_username}\nContent: {message_contents}\nAttachments: {attachments_info}",
-                    )
-                else:
-                    await user.send(
-                        f"Bookmarked: {message_link}\nAuthor: {author_username}\nContent: {message_contents}",
-                    )
+                await user.send(embed=embed)
             except (discord.Forbidden, discord.HTTPException):
                 logger.error(f"An error occurred while bookmarking {message_link} for {user}")
                 return
