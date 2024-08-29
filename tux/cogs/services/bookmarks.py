cecd4ba482c4a927bea7dd6264a0b4d6629d2957
stingleyisme <102343489+stingleyisme@users.noreply.github.com>
2024-08-28 19:22:14 -0500

    modified:   tux/cogs/services/bookmarks.py

--- tux/cogs/services/bookmarks.py
+++ tux/cogs/services/bookmarks.py
@@ -10,29 +10,46 @@ class Bookmarks(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User) -> None:
-        if str(reaction.emoji) == "🔖":
-            message_link = f"https://discord.com/channels/{reaction.message.guild.id}/{reaction.message.channel.id}/{reaction.message.id}"
-            message_contents = reaction.message.content
-            message_attachments = reaction.message.attachments
-            author_username = reaction.message.author.name
-
-            embed = EmbedCreator.create_info_embed(
-                title="Message Bookmarked",
-                description=f"> {message_contents}",
-            )
-            embed.add_field(name="Author", value=author_username, inline=False)
-            embed.add_field(name="Jump to Message", value=f"[Click Here]({message_link})", inline=False)
-
-            if message_attachments:
-                attachments_info = "\n".join([attachment.url for attachment in message_attachments])
-                embed.add_field(name="Attachments", value=attachments_info, inline=False)
-
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent) -> None:
+        if str(payload.emoji) == "🔖":
             try:
-                await user.send(embed=embed)
-            except (discord.Forbidden, discord.HTTPException):
-                logger.error(f"An error occurred while bookmarking {message_link} for {user}")
-                return
+                channel = self.bot.get_channel(payload.channel_id)
+                if channel is None:
+                    logger.error("Channel not found")
+                    return
+
+                message = await channel.fetch_message(payload.message_id)
+                if message is None:
+                    logger.error("Message not found")
+                    return
+
+                message_link = message.jump_url
+                message_contents = message.content
+                message_attachments = message.attachments
+                author_username = message.author.name
+
+                embed = EmbedCreator.create_info_embed(
+                    title="Message Bookmarked",
+                    description=f"> {message_contents}",
+                )
+                embed.add_field(name="Author", value=author_username, inline=False)
+                embed.add_field(name="Jump to Message", value=f"[Click Here]({message_link})", inline=False)
+
+                if message_attachments:
+                    attachments_info = "\n".join([attachment.url for attachment in message_attachments])
+                    embed.add_field(name="Attachments", value=attachments_info, inline=False)
+
+                try:
+                    user = self.bot.get_user(payload.user_id)
+                    if user is not None:
+                        await user.send(embed=embed)
+                    else:
+                        logger.error(f"User not found for ID: {payload.user_id}")
+                except (discord.Forbidden, discord.HTTPException) as e:
+                    logger.error(f"An error occurred while bookmarking {message_link} for {payload.user_id}: {e}")
+
+            except (discord.NotFound, discord.Forbidden, discord.HTTPException) as e:
+                logger.error(f"Failed to process the reaction: {e}")
 
 
 async def setup(bot: commands.Bot) -> None:
