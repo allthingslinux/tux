87478ec5c754130fe6079f6e90f09b4e1163e89d
wlinator <git@wlinator.org>
2024-09-04 08:20:28 -0400

    chore: Improve documentation

--- tux/database/controllers/starboard.py
+++ tux/database/controllers/starboard.py
@@ -19,6 +19,13 @@ class StarboardController:
         return await self.table.find_many()
 
     async def get_starboard_by_guild_id(self, guild_id: int) -> Starboard | None:
+        """
+        Get a starboard by guild ID.
+
+        Returns:
+        - Starboard: The starboard if found.
+        - None: If no starboard exists for the given guild ID.
+        """
         return await self.table.find_unique(where={"guild_id": guild_id})
 
     async def create_or_update_starboard(
@@ -48,6 +55,13 @@ class StarboardController:
         )
 
     async def delete_starboard_by_guild_id(self, guild_id: int) -> Starboard | None:
+        """
+        Delete a starboard by guild ID.
+
+        Returns:
+        - Starboard: The deleted starboard if found and deleted successfully.
+        - None: If no starboard exists for the given guild ID.
+        """
         return await self.table.delete(where={"guild_id": guild_id})
 
 
@@ -63,6 +77,13 @@ class StarboardMessageController:
         return guild
 
     async def get_starboard_message(self, message_id: int, guild_id: int) -> StarboardMessage | None:
+        """
+        Get a starboard message by message ID and guild ID.
+
+        Returns:
+        - StarboardMessage: The starboard message if found.
+        - None: If no starboard message exists for the given message ID and guild ID.
+        """
         return await self.table.find_unique(
             where={"message_id_message_guild_id": {"message_id": message_id, "message_guild_id": guild_id}},
         )
@@ -105,6 +126,13 @@ class StarboardMessageController:
         )
 
     async def delete_starboard_message(self, message_id: int, guild_id: int) -> StarboardMessage | None:
+        """
+        Delete a starboard message by message ID and guild ID.
+
+        Returns:
+        - StarboardMessage: The deleted starboard message if found and deleted successfully.
+        - None: If no starboard message exists for the given message ID and guild ID.
+        """
         return await self.table.delete(
             where={"message_id_message_guild_id": {"message_id": message_id, "message_guild_id": guild_id}},
         )
@@ -113,14 +141,29 @@ class StarboardMessageController:
         return await self.table.find_many(where={"message_guild_id": guild_id})
 
     async def update_star_count(self, message_id: int, guild_id: int, new_star_count: int) -> StarboardMessage | None:
+        """
+        Update the star count for a message.
+
+        Returns:
+        - StarboardMessage: The updated message if found and updated successfully.
+        - None: If the message is not found or the update fails.
+        """
         return await self.table.update(
             where={"message_id_message_guild_id": {"message_id": message_id, "message_guild_id": guild_id}},
             data={"star_count": new_star_count},
         )
 
-    async def get_starboard_message_by_id(self, original_message_id: int, guild_id: int) -> StarboardMessage | None:
+    async def get_starboard_message_by_id(self, message_id: int, guild_id: int) -> StarboardMessage | None:
         """
         Get a starboard message by its ID and guild ID.
-        This is the response by the bot, not the original message that was starred.
+
+        A "starboard message" is the response by the bot, not the original message.
+
+        Args:
+            starboard_message_id (int): The ID of the starboard message.
+            guild_id (int): The ID of the guild.
+
+        Returns:
+            StarboardMessage | None: The starboard message if found, otherwise None.
         """
-        return await self.table.find_first(where={"message_id": original_message_id, "message_guild_id": guild_id})
+        return await self.table.find_first(where={"message_id": message_id, "message_guild_id": guild_id})
