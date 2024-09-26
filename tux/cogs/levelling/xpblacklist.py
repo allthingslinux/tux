ffc7695dbf0625a87fd002ea01bd73e75a9e8e12
Atmois <atmois@atmois.com>
2024-09-26 15:14:29 +0100

    make xpblacklist command

---
+++ tux/cogs/levelling/xpblacklist.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+
+from tux.bot import Tux
+from tux.database.controllers.levelling import LevelsController
+from tux.utils import checks
+from tux.utils.flags import generate_usage
+
+
+class XpBlacklist(commands.Cog):
+    def __init__(self, bot: Tux) -> None:
+        self.bot = bot
+        self.levels_controller = LevelsController()
+        self.xp_blacklist.usage = generate_usage(self.xp_blacklist)
+
+    @commands.guild_only()
+    @checks.has_pl(2)
+    @commands.hybrid_command(
+        name="xpblacklist",
+        aliases=["rankblacklist", "levelblacklist", "rankbl", "levelbl", "xpbl"],
+    )
+    async def xp_blacklist(self, ctx: commands.Context[Tux], user: discord.User) -> None:
+        """
+        Blacklists or unblacklists a user from levelling.
+
+        Parameters
+        ----------
+        ctx : commands.Context[Tux]
+            The context object for the command.
+
+        member : discord.Member
+            The member to XP blacklist.
+        """
+        if ctx.guild is None:
+            await ctx.send("This command can only be executed within a guild.")
+            return
+
+        guild_id = ctx.guild.id
+        user_id = user.id
+
+        state = await self.levels_controller.toggle_blacklist(user_id, guild_id)
+        await ctx.send(f"{user.mention} has been {'unblacklisted' if state else 'blacklisted'}.")
+
+
+async def setup(bot: Tux) -> None:
+    await bot.add_cog(XpBlacklist(bot))
