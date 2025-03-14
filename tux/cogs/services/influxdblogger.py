ea57cd8c9177bbd32930aa922749d502d2f5a3ee
thanos <thanos@allthingslinux.org>
2025-03-14 19:39:51 -0400

    feat(services): Add influxdb logger Added a new service module that collects stats from the db and pushes them to an InfluxDB instance. this module is designed to allow collection of stats on how the bot is used in a community, in a format that can be displayed using a tool like Grafana Added dependency influxdb_client to poetry

--- tux/cogs/services/influxdblogger.py
+++ tux/cogs/services/influxdblogger.py
@@ -12,20 +12,29 @@ from tux.utils.config import CONFIG
 class InfluxLogger(commands.Cog):
     def __init__(self, bot: Tux):
         self.bot = bot
+
+    def __init__(self, bot: Tux):
+        self.bot = bot
         if self.init_influx():
             self.logger.start()
         else:
             logger.error("influxdb logger failed to init. check .env")
+            logger.error("influxdb logger failed to init. check .env")
 
+    # def cog_unload(self):
+    #    self.logger.cancel()
     # def cog_unload(self):
     #    self.logger.cancel()
     def init_influx(self):
         influx_token: str = CONFIG.INFLUXDB_TOKEN
         influx_url: str = CONFIG.INFLUXDB_URL
         self.influx_org: str = CONFIG.INFLUXDB_ORG
+        influx_token: str = CONFIG.INFLUXDB_TOKEN
+        influx_url: str = CONFIG.INFLUXDB_URL
+        self.influx_org: str = CONFIG.INFLUXDB_ORG
         if (influx_token != "") and (influx_url != "") and (self.influx_org != ""):
             write_client = InfluxDBClient(url=influx_url, token=influx_token, org=self.influx_org)
-            self.influx_write_api = write_client.write_api(write_options=SYNCHRONOUS)
+            self.influx_write_api = write_client.write_api(write_options=SYNCHRONOUS)  # pyright:ignore[reportUnknownMemberType]
             return True
         return False
 
@@ -59,12 +68,12 @@ class InfluxLogger(commands.Cog):
                 },
             )
             points: list[Point] = [
-                Point("guild stats").tag("guild", i.guild_id).field("starboard count", len(starboard_stats)),
-                Point("guild stats").tag("guild", i.guild_id).field("snippet count", len(snippet_stats)),
-                Point("guild stats").tag("guild", i.guild_id).field("afk count", len(afk_stats)),
-                Point("guild stats").tag("guild", i.guild_id).field("case count", len(case_stats)),
+                Point("guild stats").tag("guild", i.guild_id).field("starboard count", len(starboard_stats)),  # pyright:ignore[reportUnknownMemberType]
+                Point("guild stats").tag("guild", i.guild_id).field("snippet count", len(snippet_stats)),  # pyright:ignore[reportUnknownMemberType]
+                Point("guild stats").tag("guild", i.guild_id).field("afk count", len(afk_stats)),  # pyright:ignore[reportUnknownMemberType]
+                Point("guild stats").tag("guild", i.guild_id).field("case count", len(case_stats)),  # pyright:ignore[reportUnknownMemberType]
             ]
-            self.influx_write_api.write(bucket=influx_bucket, org=self.influx_org, record=points)
+            self.influx_write_api.write(bucket=influx_bucket, org=self.influx_org, record=points)  # pyright:ignore[reportUnknownMemberType]
 
 
 async def setup(bot: Tux) -> None:
