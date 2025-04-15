e77fefe712f188cd0cdcca9dc1d52970b4914f18
kzndotsh <admin@kaizen.wtf>
2025-04-15 01:55:45 -0400

    refactor(confirmation.py): add type annotations for improved code clarity and type safety
    Add type annotations to the `__init__`, `confirm`, `cancel`, and `update_button_styles` methods to enhance code readability and ensure type safety. This change helps developers understand the expected types of parameters and return values, reducing potential errors and improving maintainability.

--- tux/ui/views/confirmation.py
+++ tux/ui/views/confirmation.py
@@ -1,33 +1,34 @@
 import discord
 
-
 # Confirmation dialog view:
 # This view is to be used for a confirmation dialog.
 # ideally it should be sent as a DM to ensure the user requesting it is the only one able to interact.
 # The base class implements the buttons themselves,
 # and the subclasses, which are intended to be imported and used in cogs,
 # change the style and labels depending on severity of the action being confirmed.
+
+
 class BaseConfirmationView(discord.ui.View):
     confirm_label: str
     confirm_style: discord.ButtonStyle
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.value: bool | None = None
 
     @discord.ui.button(label="PLACEHOLDER", style=discord.ButtonStyle.secondary, custom_id="confirm")
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):  # type: ignore
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button[discord.ui.View]) -> None:
         await interaction.response.send_message("Confirming", ephemeral=True)
         self.value = True
         self.stop()
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):  # type: ignore
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button[discord.ui.View]) -> None:
         await interaction.response.send_message("Cancelling", ephemeral=True)
         self.value = False
         self.stop()
 
-    def update_button_styles(self):
+    def update_button_styles(self) -> None:
         for item in self.children:
             if isinstance(item, discord.ui.Button) and item.custom_id == "confirm":
                 item.label = self.confirm_label
@@ -35,7 +36,7 @@ class BaseConfirmationView(discord.ui.View):
 
 
 class ConfirmationDanger(BaseConfirmationView):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.confirm_label = "I understand and wish to proceed anyway"
         self.confirm_style = discord.ButtonStyle.danger
@@ -43,7 +44,7 @@ class ConfirmationDanger(BaseConfirmationView):
 
 
 class ConfirmationNormal(BaseConfirmationView):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.confirm_label = "Confirm"
         self.confirm_style = discord.ButtonStyle.green
