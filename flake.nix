ddac04f7de41b32cfcfae462dda36dee65707fa9
midirhee12 <midirhee@proton.me>
2025-05-13 17:26:38 -0500

    feat: added flake-parts & created envrc generator

--- flake.nix
+++ flake.nix
@@ -1,33 +1,63 @@
 {
   description = "All Thing's Linux discord bot - Tux";
 
-  inputs.nixpkgs = {
-    type = "github";
-    owner = "NixOS";
-    repo = "nixpkgs";
-    ref = "nixos-unstable";
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
+    flake-parts = {
+      type = "github";
+      owner = "hercules-ci";
+      repo = "flake-parts";
+      ref = "main";
+    };
   };
 
-  outputs = {
+  outputs = inputs@{
     self,
     nixpkgs,
+    flake-parts,
     ...
   }:
-    let
-      supportedSystems = [
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
       ];
 
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
-      devShells = forAllSystems (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        tux = pkgs.callPackage ./shell.nix { inherit pkgs; };
-        default = self.devShells.${system}.tux;
-      });
+      perSystem = { pkgs, self', system, ... }: {
+        devShells = {
+          default = self'.devShells.tux;
+          tux = pkgs.callPackage ./shell.nix { inherit pkgs self; };
+        };
+
+        apps.envrc = {
+          type = "app";
+          program = self'.packages.envrc;
+        };
+
+        # Creates .envrc if does not exist
+        packages.envrc = pkgs.writeShellScriptBin "envrc" ''
+          echo
+
+          if [ ! -e ".envrc" ]; then
+            echo "Creating .envrc"
+            printf "use flake .\n\n\n" | cat - .env.example > .envrc
+            echo
+
+            echo "The directory is now set up for direnv usage."
+            echo
+          else
+            echo "Please delete .envrc if you wish to recreate it."
+            echo
+          fi
+        '';
+      };
     };
 }
--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, self ? null  }:
 
 pkgs.mkShell {
+  buildInputs = if self == null then [] else [
+    self.packages.${pkgs.system}.envrc
+  ];
+
   packages = with pkgs; [
     python313
     poetry
@@ -9,10 +13,15 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    # enters the user's preferred shell using a more robust method
+    # See perSystem.packages.envrc
+    if command -v envrc >/dev/null 2>&1; then
+      envrc
+    fi
+
+    # Enters the user's preferred shell using a more robust method
     $(getent passwd $(id -un) | cut -d: -f7 | tr -d '\n')
 
-    # exits after child shell exits
+    # Exits after child shell exits
     exit
   '';
 }
