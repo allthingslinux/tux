4f69fa495a8ca953a71aa95e82a1cb6f079f15a5
thanos <thanos@allthingslinux.org>
2025-05-18 16:05:43 -0400

    feat(database): finished writing new guild schemas

--- tux/database/schemas/guild.py
+++ tux/database/schemas/guild.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
 from datetime import UTC, datetime
+from typing import Any
 
-from sqlalchemy import BigInteger, Column
+from sqlalchemy import BigInteger, Column, Index, UniqueConstraint
 from sqlmodel import Field, Relationship, SQLModel
 
 
-# TODO: fill this in with relations to other schemas after they are written
 class Guild(SQLModel, table=True):
     """Represents a guild and associated metadata"""
 
     guild_id: int = Field(primary_key=True, sa_column=Column(BigInteger))
     guild_joined_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
     config: GuildConfig = Relationship(back_populates="guild")
+    starboards: list[Starboard] | None = Relationship(back_populates="guild")
+    levels: list[Levels] | None = Relationship(back_populates="guild")
+    starboard_messages: list[StarboardMessage] | None = Relationship(back_populates="guild")
     case_count: int = Field(default=0)
 
 
@@ -46,6 +49,51 @@ class GuildConfig(SQLModel, table=True):
     guild: Guild = Relationship(back_populates="config")
 
 
+class Levels(SQLModel, table=True):
+    """Level information for a guild member"""
+
+    member_id: int = Field(primary_key=True, sa_column=Column(BigInteger))
+    xp: float = Field(default=0)
+    level: int = Field(default=0, sa_column=Column(BigInteger))
+    blacklisted: bool = Field(default=False)
+    last_message: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    guild_id: int = Field(foreign_key="guild.guild_id", primary_key=True, sa_column=Column(BigInteger))
+
+    guild: Guild = Relationship(back_populates="levels")
+
+
+class Starboard(SQLModel, table=True):
+    """Describes a starboard object for a guild"""
+
+    guild_id: int = Field(primary_key=True, sa_column=Column(BigInteger))
+    starboard_channel_id: int = Field(sa_column=Column(BigInteger))
+    starboard_emoji: str = Field(nullable=False)
+    starboard_threshold: int = Field(default=5, nullable=False)
+
+    guild: Guild = Relationship(back_populates="starboards")
+
+
+class StarboardMessage(SQLModel, table=True):
+    """Describes a starboarded message"""
+
+    message_id: int = Field(primary_key=True, sa_column=Column(BigInteger))
+    message_content: str = Field(nullable=False)
+    message_created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    message_expires_at: datetime
+    message_channel_id: int = Field(sa_column=Column(BigInteger))
+    message_user_id: int = Field(sa_column=Column(BigInteger))
+    message_guild_id: int = Field(sa_column=Column(BigInteger))
+    star_count: int = Field(default=0)
+    starboard_message_id: int = Field(nullable=False, sa_column=Column(BigInteger))
+
+    guild: Guild = Relationship(back_populates="starboard_messages")
+
+    __table_args__: Any = (
+        UniqueConstraint("message_id", "message_guild_id"),
+        Index("idx_message_id_guild_id", "message_id", "message_guild_id"),
+    )
+
+
 """ old prisma schemas, remove when no longer needed.
 model Guild {
   guild_id         BigInt             @id
@@ -62,35 +110,6 @@ model Guild {
   levels           Levels[]
 
   @@index([guild_id])
-
-model GuildConfig {
-  prefix               String?
-  mod_log_id           BigInt?
-  audit_log_id         BigInt?
-  join_log_id          BigInt?
-  private_log_id       BigInt?
-  report_log_id        BigInt?
-  dev_log_id           BigInt?
-  jail_channel_id      BigInt?
-  general_channel_id   BigInt?
-  starboard_channel_id BigInt?
-  perm_level_0_role_id BigInt?
-  perm_level_1_role_id BigInt?
-  perm_level_2_role_id BigInt?
-  perm_level_3_role_id BigInt?
-  perm_level_4_role_id BigInt?
-  perm_level_5_role_id BigInt?
-  perm_level_6_role_id BigInt?
-  perm_level_7_role_id BigInt?
-  base_staff_role_id   BigInt?
-  base_member_role_id  BigInt?
-  jail_role_id         BigInt?
-  quarantine_role_id   BigInt?
-  guild_id             BigInt  @id @unique
-  guild                Guild   @relation(fields: [guild_id], references: [guild_id])
-
-  @@index([guild_id])
-}
 
 }
 """
