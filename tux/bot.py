f57a23f78b51c33031173cff10057ca102006186
eden <eden@allthingslinux.org>
2025-06-21 19:27:02 -0400

    fix(sentry): change log lever for bot disconnection to info

--- tux/bot.py
+++ tux/bot.py
@@ -205,7 +205,7 @@ class Tux(commands.Bot):
         if sentry_sdk.is_initialized():
             with sentry_sdk.push_scope() as scope:
                 scope.set_tag("event_type", "disconnect")
-                scope.set_level("warning")
+                scope.set_level("info")
                 sentry_sdk.capture_message("Bot disconnected from Discord")
 
     # --- Sentry Transaction Tracking ---
