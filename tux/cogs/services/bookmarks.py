0a4afc9ee45aba38b7a403f66230d832633113f8
cherry <102343489+cherryl1k@users.noreply.github.com>
2025-07-26 09:31:09 -0500

    fix: Prevents tux from deleting messages outside of DMs

--- tux/cogs/services/bookmarks.py
+++ tux/cogs/services/bookmarks.py
@@ -73,8 +73,12 @@ class Bookmarks(commands.Cog):
         if payload.emoji.name in self.add_bookmark_emojis:
             await self.add_bookmark(user, message)
 
+        # Ensures were in a users DMs before removing (to fix an issue with being able to remove messages anywhere)
+        if not isinstance(channel, discord.DMChannel):
+            return
+
         # If the emoji is the remove bookmark emoji, remove the bookmark
-        elif payload.emoji.name in self.remove_bookmark_emojis:
+        if payload.emoji.name in self.remove_bookmark_emojis:
             await self.remove_bookmark(message)
 
     async def add_bookmark(self, user: discord.User, message: discord.Message) -> None:
